519ff04b950114160f5e880ee702afd4161f5a79
Sara Sales Merino <sarasame00@gmail.com>
2021-04-21 19:02:35 +0200

    Add initial grid

--- designs.js
+++ designs.js
@@ -2,6 +2,18 @@
 const submitSize = document.getElementById('submitSize');
 let canvas = document.getElementById('pixelCanvas');
 
+//loop for creating initial grid
+for(var y = 1; y <= 20; y++){ 
+    var newRow = canvas.insertRow();
+    newRow;
+    for(var x= 1; x <= 20; x++){
+        var newCell = newRow.insertCell();
+        if(x%2 == 0){
+            newCell.style.backgroundColor = '#f08000' //coloring initial griz
+        }
+    }
+}
+
 //listen to the submit size event
 submitSize.addEventListener('click', function(event){ //
     event.preventDefault();  
