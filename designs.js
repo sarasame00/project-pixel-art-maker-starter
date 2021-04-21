4bbb38281601fb8287201d3860ac6bde909dae3c
Sara Sales Merino <sarasame00@gmail.com>
2021-04-21 16:10:26 +0200

    Add listenEvent and function for creating the grid.

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+const submitSize = document.getElementById('submitSize');
+let canvas = document.getElementById('pixelCanvas');
 
-function makeGrid() {
+//listen to the submit size event
+submitSize.addEventListener('click', function(event){ //
+    event.preventDefault();  
+    let height = document.getElementById('inputHeight').value; //input value
+    let width = document.getElementById('inputWidth').value; //input value    
+    return makeGrid(height, width);
+});
 
-// Your code goes here!
+//Function for making the grid
+function makeGrid(height, width) {
+    canvas.innerHTML = ""; //clean canvas
+    for(var y= 1; y <= width; y++){ //loop for creating griz
+        var newRow = canvas.insertRow();
+        newRow;
+        for(var x= 1; x <= height; x++){
+            newRow.insertCell();
+        };
+    };
+};
 
-}
