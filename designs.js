94396dd5c27a0631bdafb6da7af3d442f3099334
Sara Sales Merino <sarasame00@gmail.com>
2021-04-22 11:16:42 +0200

    Fix height/width error. Fix eventListener(color grid). Minor changes.

--- designs.js
+++ designs.js
@@ -2,18 +2,6 @@
 const submitSize = document.getElementById('submitSize');
 let canvas = document.getElementById('pixelCanvas');
 
-//loop for creating initial grid
-for(var y = 1; y <= 20; y++){ 
-    var newRow = canvas.insertRow();
-    newRow;
-    for(var x= 1; x <= 20; x++){
-        var newCell = newRow.insertCell();
-        if(x%2 == 0){
-            newCell.style.backgroundColor = '#f08000'; //coloring initial griz
-        };
-    };
-};
-
 //listen to the submit size event
 submitSize.addEventListener('click', function(event){
     event.preventDefault();
@@ -25,20 +13,21 @@ submitSize.addEventListener('click', function(event){
 //Function for making the grid
 function makeGrid(height, width) {
     canvas.innerHTML = ""; //clean canvas
-    for(var y= 1; y <= width; y++){ //loop for creating griz
+    for(var y= 1; y <= height; y++){ //loop for creating grid
         var newRow = canvas.insertRow();
-        newRow;
-        for(var x= 1; x <= height; x++){
+        for(var x= 1; x <= width; x++){
             newRow.insertCell();
         };
     };
 };
 
 //listen to the pick color event
-canvas.addEventListener('click', pickColor);
+canvas.addEventListener('click', function(event){
+    if (event.target.nodeName === 'TD'){
+        let color = document.getElementById('colorPicker').value;
+        event.target.style.backgroundColor = color
+    }
+});
 
-//Function for changing pixels colors.
-function pickColor(evt){
-    let color = document.getElementById('colorPicker').value;
-    evt.target.style.backgroundColor = color
-}
+//initial grid
+makeGrid(20, 20);
